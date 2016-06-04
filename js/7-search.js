ac9192bf862557ee537272cd72a87caf460b624e
Игорь Теплов < >
2016-06-04 11:53:46 +0300

    1 bag

--- js/7-search.js
+++ js/7-search.js
@@ -5,7 +5,6 @@ $(function() {
     var API_KEY = '2652794-8cebb08b5e968b205ae0d687a';
     var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(input) + "&order=latest&image_type=photo";
     $.getJSON(URL, function(data) {
-      console.log(data);
       if (parseInt(data.totalHits) > 0) {
         var html = $('#template').html();
         var tmpl = _.template(html);
@@ -16,9 +15,10 @@ $(function() {
         alert('No hits:( Enter smthng else...');
     });
   }
-  $("#gallery__form").submit(function(event) {
+  $(".gallery__form").submit(function(event) {
     event.preventDefault();
-    var input = $.trim($("#gallery__input").val());
+    var input = $.trim($(".gallery__input").val());
+    console.log(input);
     if (!input) {
       alert('empty request');
     }
